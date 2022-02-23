f4adccb8cff479161af91146f8611f6368b36696
corentin <email@example.com>
2022-02-23 21:51:10 +0100

    done

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const TikTokScraper = require('tiktok-scraper');
 const https = require("https");
 const fs = require("fs");
+const ffmpeg = require('fluent-ffmpeg');
 readline = require('readline');
 
 
 let hashtags = "sneakers"
 const options = {
-    number: 2,
+    number: 20,
     sessionList: ['sid_tt=685eb894588a5f19bbf51f75c1d624c4;'],
     headers: {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
@@ -29,12 +30,10 @@ const options = {
     try {
         const posts = await TikTokScraper.hashtag(hashtags, options);
 
-
         for (const [key, value] of Object.entries(posts["collector"])) {
-
             let url = value["videoUrl"]
             https.get(url, function (res) {
-                const fileStream = fs.createWriteStream(`video${value["id"]}.mp4`);
+                const fileStream = fs.createWriteStream(`tiktok1/video${value["id"]}.mp4`);
                 res.pipe(fileStream);
                 fileStream.on("finish", function () {
                     fileStream.close();
@@ -43,6 +42,21 @@ const options = {
 
             });
         }
+        let mergedVideo = ffmpeg();
+
+        let videoNames = []
+        fs.readdirSync('tiktok1').forEach(file => {
+            videoNames.push("tiktok1/" + file)
+        });
+
+        videoNames.forEach(function (videoName) {
+            mergedVideo = mergedVideo.addInput(videoName);
+        });
+
+        mergedVideo.mergeToFile('out.mp4')
+            .on('error', function (err) {
+                console.log('Error ' + err.message);
+            })
     } catch (error) {
         console.log(error);
     }
@@ -52,19 +66,6 @@ const options = {
 
 
 
-
-
-// let url= posts
-
-// https.get(url,function(res){
-//     const fileStream = fs.createWriteStream("video.mp4");
-//     res.pipe(fileStream);
-//     fileStream.on("finish", function(){
-//         fileStream.close();
-//         console.log("done")
-//     });
-
-// });
 
 
 
